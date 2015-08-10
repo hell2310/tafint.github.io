6be493b228b62802a010902ce22e0ee800b30684
hell2310 <nguyenthanhtai.2310@gmail.com>
2015-08-10 15:07:54 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blog</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Rokkitt:400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700&subset=latin,vietnamese' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+	<header class="header-top">
+		<nav class="nav nav-bar main-nav">
+			<div class="pull-left menu-bars"><a href="#" onclick="toggleMenu()" class="fa fa-bars"></a></div>
+			<h1 id="logo" class="pull-left"><a href="#">tafint</a></h1>	
+			<ul id="primary-menu" class="pull-left hidden-sm hidden-xs">
+				<li><a href="#">HOME</a></li>
+				<li><a href="#">CATEGORY</a></li>
+				<li><a href="#">ABOUT</a></li>
+			</ul>
+			<ul class="social-menu"></ul>
+			<div class="clearfix"></div>
+		</nav>
+	</header>
+	<nav class="slide-menu">
+		<ul>
+			<li><a href="#">HOME</a></li>
+			<li><a href="#">BLOG</a></li>
+			<li><a href="#">ABOUT</a></li>
+		</ul>
+	</nav>
+	<div class="container wrapper">
+		<div class="row">
+			<article class="col-md-3 col-sm-6 post-article">
+				<div class="post">
+					<h1 class="post-title"><a href="#">Tiêu đề bài viết</a></h1>
+					<p class="post-content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse euismod dui metus. Phasellus pretium at odio eget consectetur. Nam efficitur vehicula lobortis
+					</p>
+					<p class="col-xs-6 post-view"><i class="fa fa-eye"></i> 1000</p>
+					<p class="col-xs-6 post-author"><i class="fa fa-pencil"></i> Admin</p>
+					<div class="clearfix"></div>
+				</div>
+			</article>
+			<article class="col-md-3 col-sm-6 post-article">
+				<div class="post">
+					<h1 class="post-title"><a href="#">Lorem Article post title</a></h1>
+					<p class="post-content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse euismod dui metus. Phasellus pretium at odio eget consectetur. Nam efficitur vehicula lobortis
+					</p>
+					<p class="col-xs-6 post-view"><i class="fa fa-eye"></i> 1000</p>
+					<p class="col-xs-6 post-author"><i class="fa fa-pencil"></i> Admin</p>
+					<div class="clearfix"></div>
+				</div>
+			</article>
+			<article class="col-md-3 col-sm-6 post-article">
+				<div class="post">
+					<h1 class="post-title"><a href="#">Lorem Article post title</a></h1>
+					<p class="post-content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse euismod dui metus. Phasellus pretium at odio eget consectetur. Nam efficitur vehicula lobortis
+					</p>
+					<p class="col-xs-6 post-view"><i class="fa fa-eye"></i> 1000</p>
+					<p class="col-xs-6 post-author"><i class="fa fa-pencil"></i> Admin</p>
+					<div class="clearfix"></div>
+				</div>
+			</article>
+			<article class="col-md-3 col-sm-6 post-article">
+				<div class="post">
+					<h1 class="post-title"><a href="#">Lorem Article post title</a></h1>
+					<p class="post-content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse euismod dui metus. Phasellus pretium at odio eget consectetur. Nam efficitur vehicula lobortis
+					</p>
+					<p class="col-xs-6 post-view"><i class="fa fa-eye"></i> 1000</p>
+					<p class="col-xs-6 post-author"><i class="fa fa-pencil"></i> Admin</p>
+					<div class="clearfix"></div>
+				</div>
+			</article>
+			<article class="col-md-3 col-sm-6 post-article">
+				<div class="post">
+					<h1 class="post-title"><a href="#">Lorem Article post title</a></h1>
+					<p class="post-content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse euismod dui metus. Phasellus pretium at odio eget consectetur. Nam efficitur vehicula lobortis
+					</p>
+					<p class="col-xs-6 post-view"><i class="fa fa-eye"></i> 1000</p>
+					<p class="col-xs-6 post-author"><i class="fa fa-pencil"></i> Admin</p>
+					<div class="clearfix"></div>
+				</div>
+			</article>
+			<article class="col-md-3 col-sm-6 post-article">
+				<div class="post">
+					<h1 class="post-title"><a href="#">Lorem Article post title</a></h1>
+					<p class="post-content">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse euismod dui metus. Phasellus pretium at odio eget consectetur. Nam efficitur vehicula lobortis
+					</p>
+					<p class="col-xs-6 post-view"><i class="fa fa-eye"></i> 1000</p>
+					<p class="col-xs-6 post-author"><i class="fa fa-pencil"></i> Admin</p>
+					<div class="clearfix"></div>
+				</div>
+			</article>
+		</div>
+	</div>
+	<footer id="footer-main">
+		<div class="container">
+			<div class="pull-left footer-title">Copyright 2015. All Right Reseved. Designd by tafint</div>
+			<ul class="pull-right footer-social">
+				<li class="social-facebook"><a href="#" class="fa fa-facebook"></a></li>
+				<li class="social-facebook"><a href="#" class="fa fa-google"></a></li>
+			</ul>
+		</div>
+	</footer>
+</body>
+</html>
